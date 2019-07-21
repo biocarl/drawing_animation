f9544171d71da05f63e3c20940d70d078bb5974c
Carl Hauck <cahauck@students.uni-mainz.de>
2019-07-21 11:06:52 +0200

    [refactor] Extract builder into seperate file

--- lib/src/abstract_drawing_state.dart
+++ lib/src/abstract_drawing_state.dart
@@ -6,6 +6,7 @@ import 'debug.dart';
 import 'line_animation.dart';
 import 'painter.dart';
 import 'parser.dart';
+import 'path_painter_builder.dart';
 import 'range.dart';
 import 'path_order.dart';
 
@@ -297,62 +298,5 @@ abstract class AbstractAnimatedDrawingState extends State<AnimatedDrawing> {
     // always keep paths for allAtOnce animation in original path order so we do not sort for the correct PaintOrder later on (which is pretty expensive for AllAtOncePainter)
     return this.widget.lineAnimation == LineAnimation.allAtOnce &&
         this.animationOrder != PathOrders.original;
-  }
-}
-
-class PathPainterBuilder {
-  PathPainterBuilder([LineAnimation lineAnimation]) {
-    this.lineAnimation = lineAnimation;
-  }
-  List<Paint> paints;
-  void Function(int currentPaintedPathIndex) onFinishFrame;
-  bool scaleToViewport;
-  DebugOptions debugOptions;
-  List<PathSegment> pathSegments;
-  LineAnimation lineAnimation;
-  Animation<double> animation;
-  Size customDimensions;
-
-  PathPainter build() {
-    switch (lineAnimation) {
-      case LineAnimation.oneByOne:
-        return OneByOnePainter(animation, pathSegments, customDimensions,
-            paints, onFinishFrame, scaleToViewport, debugOptions);
-      case LineAnimation.allAtOnce:
-        return AllAtOncePainter(animation, pathSegments, customDimensions,
-            paints, onFinishFrame, scaleToViewport, debugOptions);
-      default:
-        return PaintedPainter(animation, pathSegments, customDimensions, paints,
-            onFinishFrame, scaleToViewport, debugOptions);
-    }
-  }
-
-  void setAnimation(Animation<double> animation) {
-    this.animation = animation;
-  }
-
-  void setCustomDimensions(Size customDimensions) {
-    this.customDimensions = customDimensions;
-  }
-
-  void setPaints(List<Paint> paints) {
-    this.paints = paints;
-  }
-
-  void setOnFinishFrame(
-      void Function(int currentPaintedPathIndex) onFinishFrame) {
-    this.onFinishFrame = onFinishFrame;
-  }
-
-  void setScaleToViewport(bool scaleToViewport) {
-    this.scaleToViewport = scaleToViewport;
-  }
-
-  void setDebugOptions(DebugOptions debug) {
-    this.debugOptions = debug;
-  }
-
-  void setPathSegments(List<PathSegment> pathSegments) {
-    this.pathSegments = pathSegments;
   }
 }
---
+++ lib/src/path_painter_builder.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'debug.dart';
+import 'line_animation.dart';
+import 'painter.dart';
+import 'parser.dart';
+
+class PathPainterBuilder {
+  PathPainterBuilder([LineAnimation lineAnimation]) {
+    this.lineAnimation = lineAnimation;
+  }
+  List<Paint> paints;
+  void Function(int currentPaintedPathIndex) onFinishFrame;
+  bool scaleToViewport;
+  DebugOptions debugOptions;
+  List<PathSegment> pathSegments;
+  LineAnimation lineAnimation;
+  Animation<double> animation;
+  Size customDimensions;
+
+  PathPainter build() {
+    switch (lineAnimation) {
+      case LineAnimation.oneByOne:
+        return OneByOnePainter(animation, pathSegments, customDimensions,
+            paints, onFinishFrame, scaleToViewport, debugOptions);
+      case LineAnimation.allAtOnce:
+        return AllAtOncePainter(animation, pathSegments, customDimensions,
+            paints, onFinishFrame, scaleToViewport, debugOptions);
+      default:
+        return PaintedPainter(animation, pathSegments, customDimensions, paints,
+            onFinishFrame, scaleToViewport, debugOptions);
+    }
+  }
+
+  void setAnimation(Animation<double> animation) {
+    this.animation = animation;
+  }
+
+  void setCustomDimensions(Size customDimensions) {
+    this.customDimensions = customDimensions;
+  }
+
+  void setPaints(List<Paint> paints) {
+    this.paints = paints;
+  }
+
+  void setOnFinishFrame(
+      void Function(int currentPaintedPathIndex) onFinishFrame) {
+    this.onFinishFrame = onFinishFrame;
+  }
+
+  void setScaleToViewport(bool scaleToViewport) {
+    this.scaleToViewport = scaleToViewport;
+  }
+
+  void setDebugOptions(DebugOptions debug) {
+    this.debugOptions = debug;
+  }
+
+  void setPathSegments(List<PathSegment> pathSegments) {
+    this.pathSegments = pathSegments;
+  }
+}
